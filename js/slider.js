e2a095f9ce3bda6e8c72b91f1d8e8fea898aee73
Goncalo Cruz <goncalocruzprofissional@gmail.com>
2020-01-23 18:02:03 +0000

    PagInicial Finalizada
    Falta Links Produtos

---
+++ js/slider.js
@@ -0,0 +1,41 @@
+var counter=6;
+
+
+
+function changeTextBack(){
+          
+    if(counter==7){
+       document.getElementById("text4").innerHTML = "1 APP DE GESTÃO CRIADA";
+       counter=6;
+       
+    }else if(counterback==6){
+       document.getElementById("text4").innerHTML = "5 ANOS DE ESCUTEIRO";
+       counter=5;
+      
+    }else if(counterback==5){
+       document.getElementById("text4").innerHTML = "+ DE 60 IMAGENS EDITADAS NO PHOTOSHOP";
+       counter=4;
+       
+    }else if(counterback==4){
+       document.getElementById("text4").innerHTML = "3 WEBSITES REALIZADOS";
+       counter=3;  
+      
+    }else if(counterback==3){
+       document.getElementById("text4").innerHTML = "500H ESTÁGIO PROFISSIONAL";
+       counter=2;
+      
+    }else if(counterback==2){
+       document.getElementById("text4").innerHTML = "20 IMAGENS REALIZADAS NO ILLUSTRATOR";
+       counter=1;
+      
+    }else {
+       document.getElementById("text4").innerHTML = "2 PROJETOS FINALIZADOS";
+       counter=7;
+      
+    }
+  }
+
+
+
+
+
